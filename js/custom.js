12fb38fe85ae039f6ac9d48be9a797659d861466
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2020-11-16 16:35:33 +0100

    Added contact section

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 window.onscroll = function () {
   let sticker = document.getElementById("sticker");
-  if (document.body.scrollTop > 140 || document.documentElement.scrollTop > 140) {
+  if (document.body.scrollTop > 140
+      || document.documentElement.scrollTop > 140) {
     sticker.style.top = "0";
   } else {
     sticker.style.top = "-60px";
