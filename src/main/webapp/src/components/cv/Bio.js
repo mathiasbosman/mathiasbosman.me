20f9118bb67c8ff6fd77b06d3f281decf84fb547
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2021-03-29 13:47:55 +0200

    Added React suspense loading // lazy load avatar

--- src/main/webapp/src/components/cv/Bio.js
+++ src/main/webapp/src/components/cv/Bio.js
@@ -10,27 +10,28 @@ import {
 } from "@primer/components";
 import Age from "../Age";
 import {URL_FLANDERS} from "../../Constants";
-import React from "react";
+import React, {Suspense} from "react";
 
 export default class Bio extends React.Component {
   render() {
     return <Flex mx={3} className="flexWrapper">
       <Box>
         <BorderBox className="avatarContainer" mr={0}>
-          <Avatar className="avatar" itemProp="image"
-                  alt="Mathias Bosman"
-                  src="/assets/images/avatar.webp"/>
+          <Suspense fallback={<div>&nbsp;</div>}>
+            <Avatar className="avatar" itemProp="image"
+                    alt="Mathias Bosman"
+                    src="/assets/images/avatar.webp"/>
+          </Suspense>
         </BorderBox>
       </Box>
       <Box flexGrow={1}>
         <Pagehead fontSize={3} as="h1" flexGrow={1} px={3}>
-            <span itemProp="givenName">Mathias</span>&nbsp;
-            <span itemProp="familyName">Bosman</span>
+          <span itemProp="givenName">Mathias</span>&nbsp;
+          <span itemProp="familyName">Bosman</span>
 
         </Pagehead>
         <Heading as="aside" fontSize={2} mb={2} px={3}>Web designer,
-          Java
-          developer &amp; network engineer</Heading>
+          Java developer &amp; network engineer</Heading>
         <Box px={3}>
           <Text fontSize={1} as="p" itemProp="description">
             Known as "The little one", I'm a web designer and Java
@@ -38,14 +39,12 @@ export default class Bio extends React.Component {
             living in the outskirts of <Link target="_blank"
                                              itemProp="homeLocation"
                                              href="https://www.google.com/maps/place/9200+Dendermonde/">Dendermonde,
-            Belgium</Link> and
-            currently <Age birthdate="1990-12-05"/> of age. Welcome
-            to my
-            online personal resume! <img alt="Wave emoji" className="emoji"
-                                         height="1" width="1"
-                                         src="/assets/images/emoji_wave.png"/><br/>
-            At the moment I'm full-time employed at the Department
-            of
+            Belgium</Link> and currently <Age birthdate="1990-12-05"/> of age.
+            Welcome to my online personal resume! <img alt="Wave emoji"
+                                                       className="emoji"
+                                                       height="1" width="1"
+                                                       src="/assets/images/emoji_wave.png"/><br/>
+            At the moment I'm full-time employed at the Department of
             Environment of the <Link target="_blank"
                                      href={URL_FLANDERS}>Flemish
             Government</Link>.
