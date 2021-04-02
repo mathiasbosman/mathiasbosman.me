88655f59e9e505c3a7be99dd2ec8a5e1ee7ffc84
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2021-04-02 13:44:27 +0200

    #8 add error controller forward to frontend

--- src/main/java/be/mathiasbosman/cv/api/ViewController.java
+++ src/main/java/be/mathiasbosman/cv/api/ViewController.java
@@ -1,13 +1,38 @@
 package be.mathiasbosman.cv.api;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ViewController extends AbstractAppControler {
+public class ViewController extends AbstractAppControler implements ErrorController {
 
-  @GetMapping({"/blog", "/signin", "/login", "/oauth2/redirect/**"})
+  private static final String FRONTEND_PATH = "forward:";
+  private static final String ERROR_PATH = "/error";
+  private static final String ERROR_FRONTEND_PATH = "/404";
+
+
+  @GetMapping({"/blog", "/signin", "/login", "/oauth2/redirect/**", ERROR_FRONTEND_PATH})
   public String index() {
-    return "forward:";
+    return FRONTEND_PATH;
+  }
+
+
+  @RequestMapping(value = ERROR_PATH)
+  public String handleError(HttpServletRequest request) {
+    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    if (status != null) {
+      return FRONTEND_PATH + "/" + status.toString();
+    }
+    return null;
+  }
+
+
+  @Override
+  public String getErrorPath() {
+    return ERROR_PATH;
   }
 }
