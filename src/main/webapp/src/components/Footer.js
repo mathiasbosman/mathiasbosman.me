aa4657d63443093fc59daad9454eb3ad0af3be7e
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2021-04-05 17:59:53 +0200

    fix #24 footer alignment // dumbo

--- src/main/webapp/src/components/Footer.js
+++ src/main/webapp/src/components/Footer.js
@@ -8,22 +8,24 @@ export default class Footer extends React.Component {
     return <BorderBox as="footer" borderWidth={0} borderTopWidth={1} mt={5}
                       pt={5} borderRadius={0} color="text.secondary">
       <Flex justifyContent="space-between" alignItems="center">
-        <Flex justifyContent="left">
+        <Flex justifyContent="left" flex={1}>
           <Link mr={3} href="https://github.com/mathiasbosman">GitHub</Link>
           <Link mx={3}
                 href="https://www.linkedin.com/in/mathiasbosman">LinkedIn</Link>
         </Flex>
-        <Text textAlign="center">
-          <Copyright name="Mathias Bosman"/>
-        </Text>
-        <Text textAlign="right">
-          Made with<StyledOcticon ml={1} color="text.danger"
-                                  icon={HeartFillIcon}/>
-          <Link mx={1} href="https://reactjs.org/"
-                target="_blank">React</Link>
-          and <Link href="https://primer.style/"
-                    target="_blank">Primer.style</Link>
-        </Text>
+        <Flex flex={1} justifyContent="center">
+          <Text><Copyright name="Mathias Bosman"/></Text>
+        </Flex>
+        <Flex flex={1} justifyContent="right">
+          <Text textAlign="right">
+            Made with<StyledOcticon ml={1} color="text.danger"
+                                    icon={HeartFillIcon}/>
+            <Link mx={1} href="https://reactjs.org/"
+                  target="_blank">React</Link>
+            and <Link href="https://primer.style/"
+                      target="_blank">Primer.style</Link>
+          </Text>
+        </Flex>
       </Flex>
     </BorderBox>;
   }
