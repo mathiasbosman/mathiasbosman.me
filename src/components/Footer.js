15aa4291092ae19ad14f37549d5ed2141d11f077
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2021-04-16 08:30:27 +0200

    Fix footer layout once more

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,11 @@
-import {BorderBox, Flex, Link, StyledOcticon, Text} from "@primer/components";
+import {
+  BorderBox,
+  Flex,
+  Grid,
+  Link,
+  StyledOcticon,
+  Text
+} from "@primer/components";
 import React from "react";
 import Copyright from "./Copyright";
 import {HeartFillIcon} from "@primer/octicons-react";
@@ -8,19 +15,17 @@ export default class Footer extends React.Component {
     return <BorderBox as="footer" borderWidth={0} borderTopWidth={1} mt={5}
                       pt={5} borderRadius={0} color="text.secondary">
       <Flex justifyContent="space-between" alignItems="center">
-        <Flex justifyContent="left" flex={1}>
-          <Link mr={3} href="https://github.com/mathiasbosman"
+        <Flex flex={1}>
+          <Text><Copyright name="Mathias Bosman"/></Text>
+        </Flex>
+        <Grid gridTemplateColumns="repeat(3, auto)" gridGap={2} flex={1}>
+          <Link href="https://github.com/mathiasbosman"
                 target="_blank">GitHub</Link>
-          <Link mr={3}
-                href="https://www.linkedin.com/in/mathiasbosman"
+          <Link href="https://www.linkedin.com/in/mathiasbosman"
                 target="_blank">LinkedIn</Link>
-          <Link mr={3}
-                href="https://twitter.com/MathiasBosman"
+          <Link href="https://twitter.com/MathiasBosman"
                 target="_blank">Twitter</Link>
-        </Flex>
-        <Flex flex={1} justifyContent="center">
-          <Text textAlign="center"><Copyright name="Mathias Bosman"/></Text>
-        </Flex>
+        </Grid>
         <Flex flex={1} justifyContent="right">
           <Text textAlign="right">
             Made with<StyledOcticon ml={1} color="text.danger"
