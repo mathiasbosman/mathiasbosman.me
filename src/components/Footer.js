66d972ecb24ccff3f2771ad353f9b97b42eeeeb4
mathiasbosman <436498+mathiasbosman@users.noreply.github.com>
2021-08-24 19:44:51 +0200

    Add version to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import {BorderBox, Flex, Link, Text} from "@primer/components";
 import React from "react";
 import Copyright from "./Copyright";
+import packageJson from '../../package.json';
 
 export default class Footer extends React.Component {
   render() {
     return <BorderBox as="footer" borderWidth={0} borderTopWidth={1} mt={5} pt={5} borderRadius={0} color="text.secondary">
       <Flex justifyContent="space-between" alignItems="center">
         <Flex flex={1}>
-          <Text><Copyright name="Mathias Bosman"/></Text>
+          <Text><Copyright name="Mathias Bosman"/> (v. {packageJson.version})</Text>
         </Flex>
         <Flex flex={1} justifyContent="flex-end">
           <Link href="https://www.linkedin.com/in/mathiasbosman" target="_blank" mr={2}>LinkedIn</Link>
