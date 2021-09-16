0804b591c6c0a82379d6f55ec6b6fb46e60512d2
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2021-09-16 09:49:11 +0200

    Remove version from front-end. Closes #57

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import {Box, Link, Text} from "@primer/components";
 import React from "react";
 import Copyright from "./Copyright";
-import packageJson from '../../package.json';
 
 export default class Footer extends React.Component {
   render() {
@@ -10,7 +9,7 @@ export default class Footer extends React.Component {
         as="footer" borderWidth={0} borderTopWidth={1} mt={5} pt={5} color="text.secondary">
       <Box display="flex" flexWrap="wrap" justifyContent="space-between" alignItems="center">
         <Box display="flex" flexWrap="wrap" flex={1}>
-          <Text><Copyright name="Mathias Bosman"/> (v.&nbsp;{packageJson.version})</Text>
+          <Text><Copyright name="Mathias Bosman"/></Text>
         </Box>
         <Box display="flex" flexwrap="wrap" flex={1} justifyContent="flex-end">
           <Link href="https://www.linkedin.com/in/mathiasbosman" target="_blank" mr={2}>LinkedIn</Link>
