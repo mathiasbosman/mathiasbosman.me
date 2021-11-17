67c343e09072ba657c4bd71a3389a1d7e298c004
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2021-11-17 09:16:31 +0100

    New react-router-dom (v6) changed switch to routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BaseStyles, Box, ThemeProvider} from "@primer/components";
-import {Route, Switch} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
@@ -9,13 +9,13 @@ export default class App extends React.Component {
   render() {
     return <ThemeProvider colorMode="auto">
       <BaseStyles>
-      <Box className="wrapper" bg="canvas.subtle" p={3} fontSize={1}>
-          <Switch>
-            <Route path="/404" component={NotFound}/>
-            <Route exact="exact" path="/" component={Home}/>
-            <Route component={NotFound}/>
-          </Switch>
-      </Box>
+        <Box className="wrapper" bg="canvas.subtle" p={3} fontSize={1}>
+          <Routes>
+            <Route path="/404" element={<NotFound/>}/>
+            <Route exact="exact" path="/" element={<Home/>}/>
+            <Route element={<NotFound/>}/>
+          </Routes>
+        </Box>
       </BaseStyles>
     </ThemeProvider>;
   }
