2a9a64526af12cd736d3b75c850ff0ef8919b285
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-02-10 19:42:58 +0100

    Fix word wrap in title and subtitle

--- src/components/cv/Bio.js
+++ src/components/cv/Bio.js
@@ -9,20 +9,24 @@ export default class Bio extends React.Component {
       <Box mx={"auto"} py={2}>
         <Box
             borderWidth="1px" borderStyle="solid" borderColor="border.default"
-            borderRadius="50%" >
-          <Avatar size="150" src="/assets/images/mathias.png" />
+            borderRadius="50%">
+          <Avatar size="150" src="/assets/images/mathias.png"/>
         </Box>
       </Box>
       <Box flexGrow={1}>
-        <Pagehead as="h1" sx={{flexGrow: 1, p: [0,0,3], textAlign: ["center", "center", "left"]}}>
+        <Pagehead as="h1" sx={{
+          flexGrow: 1,
+          p: [0, 0, 3],
+          textAlign: ["center", "center", "left"]
+        }}>
           <Text fontSize={5} fontWeight="bold"
-                itemProp="givenName">Mathias</Text>&nbsp;
-          <Text fontSize={5} fontWeight="bold"
-                itemProp="familyName">Bosman</Text>
+                itemProp="givenName">Mathias</Text>&nbsp;<Text fontSize={5}
+                                                               fontWeight="bold"
+                                                               itemProp="familyName">Bosman</Text>
         </Pagehead>
-        <Heading sx={{fontSize: 2, mb: 2, px: [0,0,3]}}>Web designer,
-          Java developer &amp; network engineer</Heading>
-        <Box px={[0,0,3]}>
+        <Heading sx={{fontSize: 2, mb: 2, px: [0, 0, 3]}}>Web&nbsp;designer,
+          Java&nbsp;developer &amp; network&nbsp;engineer</Heading>
+        <Box px={[0, 0, 3]}>
           <Text as="p" itemProp="description">
             Known as &quot;The little one&quot;, I&apos;m a web designer and
             Java
