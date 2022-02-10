c6a8a507f38e8f5ff67a2e3788e12a3799bf1ed8
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-02-10 19:43:36 +0100

    Formatting of Bio.js

--- src/components/cv/Bio.js
+++ src/components/cv/Bio.js
@@ -29,9 +29,7 @@ export default class Bio extends React.Component {
         <Box px={[0, 0, 3]}>
           <Text as="p" itemProp="description">
             Known as &quot;The little one&quot;, I&apos;m a web designer and
-            Java
-            developer
-            living in <span itemProp="homeLocation">Lebbeke, Belgium</span> and
+            Java developer living in <span itemProp="homeLocation">Lebbeke, Belgium</span> and
             currently <Age birthdate="1990-12-05"/> of age.
             Welcome to my online personal resume!<br/>
             At the moment I&apos;m full-time employed at the Department of
