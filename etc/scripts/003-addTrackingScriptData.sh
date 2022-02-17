fb0ac54ddf401b066c4ce38e4ef5296f509e6692
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-02-17 11:52:30 +0100

    Reverting this; as it gets added to the index file it's visible anyway

--- etc/scripts/003-addTrackingScriptData.sh
+++
@@ -1,7 +0,0 @@
-indexFile="./public/index.html";
-
-echo "Adding Matomo base url";
-sed -i -e "s/{{MATOMO_BASE_URL}}/$SECRET_MATOMO_BASE_URL/gi" $indexFile;
-
-echo "Adding Matomo site id";
-sed -i -e "s/{{MATOMO_SITE_ID}}/$SECRET_MATOMO_SITE_ID/gi" $indexFile;
