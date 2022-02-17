919aca72cebdb4110bcacf15a39c5a475257b9ab
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-02-17 21:54:31 +0100

    Move scripts to root folder and merge them into one pre-install script

--- etc/scripts/001-updateLastMod.sh
+++
@@ -1,6 +0,0 @@
-sitemapFile="./public/sitemap.xml";
-placeholder="{{LAST_MOD}}";
-currentDate=$(date -u +"%Y-%m-%dT%H:%M:%SZ");
-
-echo "Updating sitemap lastmod date to: $currentDate";
-sed -i -e "s/$placeholder/$currentDate/gi" $sitemapFile;
--- etc/scripts/002-setContactEmail.sh
+++
@@ -1,5 +0,0 @@
-constantsFile="./src/Constants.js";
-placeholder="{{CONTACT_EMAIL}}";
-
-echo "Setting contact email in $constantsFile";
-sed -i -e "s/$placeholder/$SECRET_CONTACT_EMAIL/gi" $constantsFile;
