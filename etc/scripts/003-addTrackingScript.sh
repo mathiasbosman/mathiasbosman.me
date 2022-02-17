74039e7fa828f275f5601733909bd0206ddd51c4
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-02-17 11:45:58 +0100

    Header script changes

--- etc/scripts/003-addTrackingScript.sh
+++
@@ -1,6 +0,0 @@
-indexFile="./public/index.html";
-toReplace="<!-- header scripts -->";
-replacement="$SECRET_HEADER_SCRIPTS";
-
-echo "Adding header scripts (if any)";
-sed -i -e "s/$toReplace/$replacement/gi" $indexFile;
