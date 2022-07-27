440a7652c602e8c4f6d0c05176fea50f7108c4b6
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-07-27 15:26:00 +0200

    #207 removed cnr and revision number

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -11,8 +11,8 @@ export default class Disclaimer extends React.Component {
         borderTopWidth={[1, 1, 0]}
         borderStyle="solid" borderColor="border.default"
         flex={1} display="flex" justifyContent="center" mx="auto" p={3} maxWidth={LAYOUT_WIDTH}>
-      <Text fontSize={0} color={"fg.muted"}>
-        <Copyright name={packageJson.author}/> - rev {packageJson.version} {packageJson.organisationNumber ? ' - ' + packageJson.organisationNumber : ''}
+      <Text fontSize={0} color={"fg.muted"} textAlign={"center"}>
+        <Copyright name={packageJson.author}/>
       </Text>
     </Box>;
   }
