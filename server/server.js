4740b55f5690d7096951300eb7312e45bf6a6d66
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-08-12 07:58:03 +0200

    #211 move https to front?

--- server/server.js
+++ server/server.js
@@ -4,29 +4,9 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'build');
-app.use(express.static(publicPath));
-
-// set up rate limiter: maximum of five requests per minute
-const RateLimit = require('express-rate-limit');
-const limiter = new RateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 100
-});
-app.use(limiter);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
-});
-
 console.log(`Server node is ${process.env.NODE_ENV}!`);
 if(process.env.NODE_ENV === 'production') {
   console.log(`Server will redirect to https`);
-
   app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https')
       res.redirect(`https://${req.header('host')}${req.url}`)
@@ -34,3 +14,23 @@ if(process.env.NODE_ENV === 'production') {
       next()
   })
 }
+
+const publicPath = path.join(__dirname, '..', 'build');
+app.use(express.static(publicPath));
+
+// set up rate limiter: maximum of five requests per minute
+const RateLimit = require('express-rate-limit');
+const limiter = new RateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100
+});
+app.use(limiter);
+
+// make sure everything is routed to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
