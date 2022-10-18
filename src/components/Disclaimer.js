474b1caaaaddb44900843565e8881818bf80aa94
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-10-18 16:37:41 +0200

    Toevoegen btw nummer

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -13,6 +13,7 @@ export default class Disclaimer extends React.Component {
         flex={1} display="flex" justifyContent="center" mx="auto" p={3} maxWidth={LAYOUT_WIDTH}>
       <Text fontSize={0} color={"fg.muted"} textAlign={"center"}>
         <Copyright name={packageJson.author} version={packageJson.version}/>
+        &nbsp; BTW BE 0737.612.645
       </Text>
     </Box>;
   }
