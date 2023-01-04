06c315e351b2e0be949630eabdc4809573f3bbfd
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-01-04 09:25:23 +0100

    #245 move component test to Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    video: false,
+    baseUrl: "http://localhost:3000",
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
   },
 });
