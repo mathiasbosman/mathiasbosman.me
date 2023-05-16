d62a16ae3cf6572f39eab5ad08f93abf46f033d4
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-05-16 22:07:15 +0200

    Add certificates and awards

--- src/components/cv/Awards.tsx
+++ src/components/cv/Awards.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import {Box, Heading, Link, Text} from "@primer/react";
+import {Box, Heading, Link} from "@primer/react";
+import {ChecklistIcon, TrophyIcon} from "@primer/octicons-react";
 
 const Awards = () => (
     <Box as="section" mt={5}>
-      <Heading as="h2" sx={{textAlign: ["center", "center", "left"]}}>Awards</Heading>
-      <Box
-          borderWidth="1px" borderStyle="solid" borderColor="border.default" borderRadius={2}
-          p={3}>
-        <Text>2011 ·</Text>
-        <Link sx={{mx: 1, fontWeight: "bold"}} href="https://cera-award.be/" target="_blank">CERA Award</Link>
-        <Box>
-          <Text>
-            During my high school education our project team won the CERA Award for
-            creating a PHP web portal for evaluating employees.
-          </Text>
+      <Heading as="h2" sx={{textAlign: ["center", "center", "left"]}}>Awards &amp; certificates</Heading>
+      <Box sx={{display: "flex", flexDirection: "column", justifyContent: "space-around"}}>
+
+        <Box sx={{mt: 2}}>
+          <ChecklistIcon/> 2023 Mastering IT management
+          (<Link aria-label="Link to Vlerick Business School website" href="https://www.vlerick.com/en/" target="_blank">Vlerick Business School</Link>)
+        </Box>
+
+        <Box sx={{mt: 2}}>
+          <TrophyIcon/> 2011 <Link aria-label="Link to CERA award website"
+                                   href="https://cera-award.be/"
+                                   target="_blank">CERA award</Link>
         </Box>
       </Box>
     </Box>
