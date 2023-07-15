fa20d89bbbad912c30caea6dd24a6eed934939d5
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-07-15 17:49:56 +0200

    refactor: #312 (self-)review
    Remove safelist from tailwind (creates big chunk while building!)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,css,scss,js,ts}"],
-  safelist: [
-    // done because we use these classes dynamically
-    { pattern: /w-.+/ },
-    { pattern: /h-.+/ },
-  ],
   theme: {
     extend: {},
   },
