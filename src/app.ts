76161fb56b6adbf5fb96e62b7eee7c6a3c9d4684
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-07-17 08:23:08 +0200

    let's try netfliy then

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import { customElement } from "lit/decorators.js";
 import { TailwindElement } from "./shared/tailwind.element.ts";
 import { router, setRoutes } from "./router.ts";
+import type { PropertyValues } from "lit";
 import { html, type TemplateResult } from "lit";
 
 @customElement("sandbox-app")
 export class SandboxApp extends TailwindElement {
-  override async firstUpdated(): Promise<void> {
+  override async firstUpdated(_changedProps: PropertyValues): Promise<void> {
+    super.firstUpdated(_changedProps);
     await this.initRouter();
   }
 
   protected override render(): TemplateResult {
-    return html` <div id="app-content" class="h-full"><slot></slot></div> `;
+    return html` <div id="app-content" class="h-full"></div> `;
   }
 
   private async initRouter(): Promise<void> {
@@ -18,6 +20,7 @@ export class SandboxApp extends TailwindElement {
     await setRoutes();
   }
 }
+
 declare global {
   interface HTMLElementTagNameMap {
     "sandbox-app": SandboxApp;
