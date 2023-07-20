6b64a0cb9489ec738f99bfe90d3e6b941ac9a60b
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-07-20 14:46:08 +0200

    Add escapeHtml method

--- test/driver/vitest/shared/utils.test.ts
+++ test/driver/vitest/shared/utils.test.ts
@@ -1,7 +1,11 @@
 import { describe, it, expect } from 'vitest'
-import { type Period, renderPeriodYearString } from '../../../../src/shared/utils.tsx'
+import {
+  escapeHtml,
+  type Period,
+  renderPeriodYearString
+} from '../../../../src/shared/utils.tsx'
 
-describe('renderPeriodYearString', () => {
+describe('renderPeriodYearString()', () => {
   it('Only renders the year if same year', () => {
     const period: Period = {
       from: new Date('2020-01-01'),
@@ -23,5 +27,23 @@ describe('renderPeriodYearString', () => {
       from: new Date('2020-01-01')
     }
     expect(renderPeriodYearString(period, 'foo')).eq('2020 - foo')
+  })
+})
+
+describe('escapeHtml()', () => {
+  it('Empty strings returns empty', () => {
+    expect(escapeHtml('')).eq('')
+  })
+
+  it('Normal strings returs as-is', () => {
+    expect(escapeHtml('foo bar')).eq('foo bar')
+  })
+
+  it('String with special character gets escaped', () => {
+    expect(escapeHtml('foo "bar"')).eq('foo &quot;bar&quot;')
+    expect(escapeHtml('foo & bar')).eq('foo &amp; bar')
+    expect(escapeHtml("foo 'bar'")).eq('foo &#39;bar&#39;')
+    expect(escapeHtml('foo < bar')).eq('foo &lt; bar')
+    expect(escapeHtml('foo > bar')).eq('foo &gt; bar')
   })
 })
