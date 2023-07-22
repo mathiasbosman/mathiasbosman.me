5958512e86759fb3d3bbd5a7b51b81a978a77ba7
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-07-22 10:41:12 +0200

    refactor: format scripts dir

--- scripts/postBuild.cjs
+++ scripts/postBuild.cjs
@@ -1,21 +1,21 @@
-const { readFile, writeFile } = require(`fs`);
-const file = `./dist/sitemap.xml`;
-const encoding = `utf-8`;
+const { readFile, writeFile } = require(`fs`)
+const file = `./dist/sitemap.xml`
+const encoding = `utf-8`
 
 readFile(file, encoding, function (err, contents) {
   if (err) {
-    console.log(err);
-    return;
+    console.log(err)
+    return
   }
 
-  const currentDate = `<lastmod>` + new Date().toISOString() + `</lastmod>`;
-  const replaced = contents.replace(/<lastmod>.+<\/lastmod>/g, currentDate);
+  const currentDate = `<lastmod>` + new Date().toISOString() + `</lastmod>`
+  const replaced = contents.replace(/<lastmod>.+<\/lastmod>/g, currentDate)
 
   writeFile(file, replaced, encoding, function (err) {
     if (err) {
-      console.log(err);
+      console.log(err)
     } else {
-      console.log(`${file} now contains ${currentDate}`);
+      console.log(`${file} now contains ${currentDate}`)
     }
-  });
-});
+  })
+})
