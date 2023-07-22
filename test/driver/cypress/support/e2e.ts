eeae21ca2d21e3c0b551f02d1ed72081f46d851d
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-07-22 11:16:14 +0200

    fix: e2e outgoing link test

--- test/driver/cypress/support/e2e.ts
+++ test/driver/cypress/support/e2e.ts
@@ -1,17 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-argument */
+interface testedLink {
+  href: string,
+  test: string
+}
+
 export const testOutgoingLinks = () => {
-  it(`Outgoing links do not return a 404 status`, () => {
-    const checkedLinks: string[] = []
-    cy.get(`a`).each((link) => {
-      const href = link.prop(`href`)
-      if (!checkedLinks.includes(href)) {
-        cy.request({
-          failOnStatusCode: false,
-          url: href,
-        })
-        .its(`status`)
-        .should(`not.eq`, 404)
-        checkedLinks.push(href)
-      }
-    })
+  const checkedLinks: testedLink[] = []
+  cy.get(`a[href^="http://"],a[href^="https://"]`).each((link) => {
+    const testingLink: testedLink = {
+      href: link.prop(`href`),
+      test: Cypress.currentTest.title
+    }
+    const alreadyChecked = checkedLinks.find(
+      link => link.href === testingLink.href)
+
+    if (alreadyChecked === undefined) {
+      cy.request({
+        failOnStatusCode: false,
+        url: testingLink.href,
+      })
+      .its(`status`)
+      .should(`not.eq`, 404)
+      checkedLinks.push(testingLink)
+    } else {
+      cy.log(alreadyChecked.href + ' already tested in ' + alreadyChecked.test)
+    }
   })
 }
