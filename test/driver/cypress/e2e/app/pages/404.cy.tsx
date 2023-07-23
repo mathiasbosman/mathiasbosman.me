bf4cdea62d9a44c2a9334a3c06523ed2453da939
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-07-23 18:28:22 +0200

    refactor: use same links as navigation in the footer

--- test/driver/cypress/e2e/app/pages/404.cy.tsx
+++ test/driver/cypress/e2e/app/pages/404.cy.tsx
@@ -1,14 +1,7 @@
-import { testOutgoingLinks } from '../../../support/e2e.ts'
-
 describe('Not found page tests', () => {
   it('404 page is available', () => {
     cy.visit('/404')
     cy.screenshot()
-  })
-
-  it('Outgoing links do not return a 404 status', () => {
-    cy.visit('/404')
-    testOutgoingLinks()
   })
 
   it('None existing route gives 404', () => {
