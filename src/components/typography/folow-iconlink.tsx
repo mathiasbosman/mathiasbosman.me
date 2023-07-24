ee8673e3e9dc4716ad434092191591d04cfcc397
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-07-24 12:46:01 +0200

    FollowIconLink now accepts pre- and postfixes

--- src/components/typography/folow-iconlink.tsx
+++ src/components/typography/folow-iconlink.tsx
@@ -6,9 +6,14 @@ import type { HTMLSimpleLink } from '../../shared/utils.tsx'
 interface Props {
   link: HTMLSimpleLink
   icon: Icon
+  prefix?: string
+  postfix?: string
 }
 
 export const FollowIconLink = (props: PropsWithChildren<Props>): ReactElement => {
+
+  const prefix = props.prefix ?? 'Follow me on '
+
   return <a
     className={'group flex text-sm font-medium text-zinc-800 transition hover:text-indigo-500 dark:text-zinc-200 dark:hover:text-indigo-500'}
     href={props.link.href}
@@ -18,7 +23,7 @@ export const FollowIconLink = (props: PropsWithChildren<Props>): ReactElement =>
       className={'h-5 w-5 flex-none fill-zinc-500 transition group-hover:fill-indigo-500'}>
       <path fillRule={'evenodd'} clipRule={'evenodd'} d={props.icon.svgPath}></path>
     </svg>
-    <span className={'ml-4'}>Follow me on {props.link.text}</span>
+    <span className={'ml-4'}>{prefix}{props.link.text}{props.postfix}</span>
   </a>
 }
 
--- test/driver/cypress/component/typography/folow-iconlink.cy.tsx
+++ test/driver/cypress/component/typography/folow-iconlink.cy.tsx
@@ -12,6 +12,15 @@ describe('<FollowIconLink/>', () => {
     cy.get('span').should('have.text', 'Follow me on foo bar')
   })
 
+  it('Renders with given pre- and postfix', () => {
+    cy.mount(<FollowIconLink
+      prefix={'Join me on '}
+      postfix={' for more!'}
+      link={htmlSimpleLinkFixture('/href', 'foo bar')}
+      icon={LinkIcon} />)
+    cy.get('span').should('have.text', 'Join me on foo bar for more!')
+  })
+
   it('Line-height of text should equal height of icon', () => {
     cy.mount(<FollowIconLink link={htmlSimpleLinkFixture('/href', 'text')} icon={LinkIcon}/>)
     cy.get('svg').invoke('css', 'height').then(iconHeight => {
