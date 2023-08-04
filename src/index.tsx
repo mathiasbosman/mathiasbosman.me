0de3ba465a2e119318b7a9d5c60d7a8ba53ebc16
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-08-04 14:32:10 +0200

    Remove analytics.. useless stuff

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/app.tsx'
 import '@/index.css'
-import { Analytics } from '@vercel/analytics/react'
 
-// Inject React + add Vercel analytics
+// Inject React
 ReactDOM.createRoot(document.getElementById('app-root')!).render(
   <React.StrictMode>
     <App/>
-    <Analytics/>
   </React.StrictMode>
 )
