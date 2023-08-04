97b2bfcc7949bea15bf60f958bebddcdb4e3ffbe
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-08-04 12:34:01 +0200

    fix: #384 remove Link from Avatar component

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,6 +1,5 @@
 import type { PropsWithChildren, ReactElement } from 'react'
 import type { HTMLImage } from '@shared/utils.ts'
-import { Link } from 'react-router-dom'
 
 export enum AvatarSize {
   xs = 9,
@@ -13,29 +12,27 @@ interface Props {
 }
 
 export const Avatar = (props: PropsWithChildren<Props>): ReactElement => {
-  function _renderAvatarLink (css: string): ReactElement {
-    return <Link aria-label={'Home'} className={css + ' pointer-events-auto block'} to={'/'}>
-      <img
+  function _renderImage (): ReactElement {
+    return <img
         loading={'lazy'}
         alt={props.image.alt}
         decoding={'async'}
         src={props.image.src}
         style={{ color: 'transparent' }}
-        className={css + ' rounded-full object-cover bg-zinc-100 dark:bg-zinc-800'}
+        className={'aspect-square rounded-full object-cover bg-zinc-100 dark:bg-zinc-800'}
       />
-    </Link>
   }
 
   switch (props.size) {
     case AvatarSize.s:
       return <div
         className={'w-16 h-16 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10'}>
-        {_renderAvatarLink('w-16 h-16')}
+        {_renderImage()}
       </div>
     case AvatarSize.xs:
       return <div
-        className={'w-9 h-9 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10'}>
-        {_renderAvatarLink('w-9 h-9')}
+        className={'w-11 h-11 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10'}>
+        {_renderImage()}
       </div>
   }
 }
