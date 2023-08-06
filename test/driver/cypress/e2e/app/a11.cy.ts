04b4b3f9fca223b3b184e31af60b06137a6515e3
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-08-06 19:59:13 +0200

    Nav isn't visible on smaller vp

--- test/driver/cypress/e2e/app/a11.cy.ts
+++ test/driver/cypress/e2e/app/a11.cy.ts
@@ -10,11 +10,12 @@ describe("A11 checks", () => {
   });
 
   it("All pages have an h1, header, main nav and footer", () => {
+    cy.viewport(601, 962);
     allPages.forEach((page) => {
       cy.visit(page);
       cy.get("h1").should("be.visible");
       cy.get("main").should("be.visible");
-      cy.get("nav").filter(":visible").should("exist");
+      cy.get("nav").should("exist");
       cy.get("header").should("be.visible");
       cy.get("footer").should("be.visible");
       cy.screenshot("pages" + page);
