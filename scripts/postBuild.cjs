e041b91b88b27d0860b8e5d16f579e108d6974d2
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-08-13 09:39:48 +0200

    chore: 404 status code (#398)

--- scripts/postBuild.cjs
+++ scripts/postBuild.cjs
@@ -1,8 +1,10 @@
-const { readFile, writeFile } = require(`fs`);
-const file = `./dist/sitemap.xml`;
+const { readFile, writeFile, copyFile } = require(`fs`);
+const buildDir = "./dist/";
 const encoding = `utf-8`;
 
-readFile(file, encoding, function (err, contents) {
+// update <lastMod> in the sitemap
+const sitemap = buildDir + "sitemap.xml";
+readFile(sitemap, encoding, function (err, contents) {
   if (err) {
     console.log(err);
     return;
@@ -11,11 +13,19 @@ readFile(file, encoding, function (err, contents) {
   const currentDate = `<lastmod>` + new Date().toISOString() + `</lastmod>`;
   const replaced = contents.replace(/<lastmod>.+<\/lastmod>/g, currentDate);
 
-  writeFile(file, replaced, encoding, function (err) {
+  writeFile(sitemap, replaced, encoding, function (err) {
     if (err) {
-      console.log(err);
+      throw err;
     } else {
-      console.log(`${file} now contains ${currentDate}`);
+      console.log(`${sitemap} now contains ${currentDate}`);
     }
   });
+});
+
+// copy index.html to 404.html
+copyFile(buildDir + "index.html", buildDir + "404.html", (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Index file copied to 404");
 });
