099dd414972848811d22b4331b36c4767ff5f514
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-08-14 19:12:16 +0200

    debug: check process.env

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,17 @@
 import type { ReactElement } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "@models/config/navigation.config.tsx";
-import packageJson from "../package.json";
 
 const router = createBrowserRouter(routes);
 
 export const App = (): ReactElement => {
 
-  alert(window.location.origin);
-  alert(packageJson.homepage);
+  console.log(process.env);
 
   return (
     <>
-      {window.location.origin.includes(packageJson.homepage) && (
-        <p className={"sr-only"}>test</p>
+      {window.location.origin.includes("mathiasbosman-me") && (
+        <p className={"sr-only"}>dummy-analytics</p>
       )}
       <RouterProvider router={router} />
     </>
