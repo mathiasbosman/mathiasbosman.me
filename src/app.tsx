8fae9f70479acaa686c744010ea912bcbf78cacc
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-08-14 19:19:06 +0200

    feature: analytics with Matomo (#399)

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,19 @@
 import type { ReactElement } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "@models/config/navigation.config.tsx";
+import packageInfo from "../package.json";
+import { Analytics } from "@components/analytics.tsx";
 
 const router = createBrowserRouter(routes);
 
 export const App = (): ReactElement => {
   return (
     <>
+      {window.location.origin.includes(packageInfo.homepage) && (
+        <Analytics
+          endpoint={"https://stats.beanleaf.be/js/container_FpHyoHNp.js"}
+        />
+      )}
       <RouterProvider router={router} />
     </>
   );
