3a330accaedcb2d4808595982245a92ce0cbffa2
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-08-27 12:39:55 +0200

    chore: remove Plausible analytics

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,11 @@
 import type { ReactElement } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "@models/config/navigation.config.tsx";
-import { PlausibleAnalytics } from "@components/analytics.tsx";
 
 const router = createBrowserRouter(routes);
 
 export const App = (): ReactElement => {
-  return (
-    <>
-      <PlausibleAnalytics
-        domain={"mathiasbosman.me"}
-        endpoint={"https://plausible.io/js/script.js"}
-      />
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
