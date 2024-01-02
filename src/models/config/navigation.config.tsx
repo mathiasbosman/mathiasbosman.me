4667d87fdbad00d90b89b82eae7e03cb2eb3493b
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2024-01-02 12:42:53 +0100

    chore: 2024 update update license and correct typo in README.md

--- src/models/config/navigation.config.tsx
+++ src/models/config/navigation.config.tsx
@@ -14,6 +14,19 @@ interface RouteLink {
   errorElement?: React.ReactElement;
 }
 
+/**
+ * Configuration for the routes in the application.
+ * Each route is represented as an object with the following properties:
+ * - link: An object representing the link for the route. It has two properties:
+ *   - href: The URL path for the route.
+ *   - text: The text to display for the link.
+ * - element: The React component to render for the route.
+ * - isPublic: A boolean indicating whether the route is public. Optional.
+ * - errorElement: The React component to render if there is an error. Optional.
+ *
+ * @see vercel.json for the configuration of the routes in the Vercel deployment.
+ * @type {RouteLink[]}
+ */
 export const routeConfig: RouteLink[] = [
   {
     link: { href: "/", text: "Home" },
