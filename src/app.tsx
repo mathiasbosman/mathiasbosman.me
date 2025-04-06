44a951a4cd1e07efedbdaec8ed5788d763550d2f
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2025-04-06 19:34:12 +0200

    Add Inter as font

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
 import type { ReactElement } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from '@models/config/navigation.config.tsx';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/400-italic.css';
+import '@fontsource/inter/700.css';
 
 const router = createBrowserRouter(routes);
 
