9e61900b478782349eb9a2ef7f852fd05644b04d
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2025-04-08 19:23:24 +0200

    Lazy load routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,27 @@
 import type { ReactElement } from 'react';
+import { Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from '@models/config/navigation.config.tsx';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/400-italic.css';
 import '@fontsource/inter/700.css';
+import { PageLoader } from '@components/page-loader.tsx';
+import { DelayedFallback } from '@components/common/delayed-fallback.tsx';
 
 const router = createBrowserRouter(routes);
 
 export const App = (): ReactElement => {
-  return <RouterProvider router={router} />;
+  return (
+    <Suspense
+      fallback={
+        <DelayedFallback>
+          <PageLoader />
+        </DelayedFallback>
+      }
+    >
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 };
 
 export default App;
